7afcda9e9116048660dffd8058255b2179973b8b
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2022-10-12 19:40:16 +0200

    Fill all classes in Domain with properties + Fill the LocalDbContext

--- src/Data/LocalDbContext.cs
+++ src/Data/LocalDbContext.cs
@@ -7,6 +7,16 @@ namespace Dot.Net.WebApi.Data
 {
     public class LocalDbContext : DbContext
     {
-        public DbSet<User> Users { get; set;}
+
+        public LocalDbContext (DbContextOptions<LocalDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<BidList> BidList { get; set; }
+        public DbSet<CurvePoint> CurevPoint { get; set; }
+        public DbSet<Rating> Rating { get; set; }
+        public DbSet<RuleName> RuleName { get; set; }
+        public DbSet<Trade> Trade { get; set; }
     }
 }
--- src/Domain/Rating.cs
+++ src/Domain/Rating.cs
@@ -1,7 +1,13 @@
-namespace Dot.Net.WebApi.Controllers.Domain
+//namespace Dot.Net.WebApi.Controllers.Domain
+namespace Dot.Net.WebApi.Domain
 {
     public class Rating
     {
         // TODO: Map columns in data table RATING with corresponding fields
+        public int Id { get; set; }
+        public string MoodysRating { get; set; }
+        public string SandPRating { get; set; }
+        public string FitchRating { get; set; }
+        public int OrderNumber { get; set; }
     }
 }
